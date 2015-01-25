04eb8491d76375ef20d538198d60f54dd68dfaea
Brett Morrison <Brett.Morrison@gmail.com>
2015-01-25 13:41:35 -0700

    Completed homework assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions inveter a matrix and cache it to save time when 
+## the inversion of the matrix is needed again. 
 
-## Write a short comment describing this function
+
+## Creates a matrix oject that can cache it's own inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+   
 }
 
-
-## Write a short comment describing this function
+## Calculates the inverse of the makeCacheMatrix function. If the inverse has 
+## already been calculated for the matrix, then cacheSolve will retrieve 
+## the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+  m <- x$getinverse() 
+  ## If the matrix exists, don't compute it, just retrieve it
+  if(!is.null(m)) {
+    message("getting cached inverse of the matrix")
+    return(m)
+  }
+  ## If the inverse of the matix doesn't exist, use solve() to invert it 
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+
+  
+  
 }
---
+++ example.R
@@ -0,0 +1,30 @@
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  
+  start_time <- Sys.time()
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+  end_time <- Sys.time()
+  time_duration <- end_time - start_time
+  print(time_duration)
+}
